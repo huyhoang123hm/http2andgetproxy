25a1bbcf77583249427f8992d20aa193197e8110
huyhoang123hm <78525207+huyhoang123hm@users.noreply.github.com>
2023-07-18 21:16:40 +0700

    Update b.py

--- b.py
+++ b.py
@@ -1,67 +1,62 @@
 import threading
 import socks
 import ssl
-import time
 import sys
+
 target_host = "ipv4.icanhazip.com"
 target_port = 80
-proxy_type_list = {
-    "http": socks.HTTP,
-    "socks4": socks.SOCKS4,
-    "socks5": socks.SOCKS5
-}
-proxy_type = sys.argv[1]
-pysocks_proxy_type = proxy_type_list[proxy_type]
-input_file = "proxy.txt"
-output_file = "proxy.txt"
-timeout = 5
-total_socks = [proxy.strip() for proxy in open(input_file)]
-live_socks = []
-live_socks_counter = 0
-def connect_socks(socks_address):
-    global live_socks_counter
-    dead_socks_counter = 0
-    headers = "GET / HTTP/1.1\r\nHost: " + target_host + "\r\n\r\n"
-    socks_address_split = socks_address.split(":")
-    socks_host = socks_address_split[0]
-    socks_port = int(socks_address_split[1])
-    try:
-        socks.setdefaultproxy(pysocks_proxy_type, socks_host, socks_port)
-    except:
-        total_socks.remove(socks_address)
-        return
-    while True:
-        if dead_socks_counter == 3:
-            total_socks.remove(socks_address)
-            break
-        try:
-            conn = socks.socksocket()
-            conn.settimeout(timeout)
-            conn.connect((target_host, target_port))
-            if target_port == 443:
-                ssl_context = ssl.SSLContext()
-                conn = ssl_context.wrap_socket(conn, server_hostname=target_host)
-            conn.send(headers.encode())
-            conn.close()
-            live_socks.append(socks_address)
-            live_socks_counter += 1
-            print('Total HTTP working: ' + str(live_socks_counter), end='\r')
-            break
-        except:
-            conn.close()
-            dead_socks_counter += 1
+proxy_type_list = {"http": socks.HTTP, "socks4": socks.SOCKS4, "socks5": socks.SOCKS5}
 
-thread_list = []
-for socks_address in total_socks:
-    thread = threading.Thread(target=connect_socks, args=(socks_address, ))
-    thread.start()
-    thread_list.append(thread)
-    sys.stdout.flush()
-for thread in thread_list:
-    thread.join()
-    sys.stdout.flush()
-print('Total SOCKS working: ' + str(live_socks_counter))
-file = open(output_file, "w")
-for socks_address in live_socks:
-    file.write(socks_address + "\n")
-file.close()
+def check_socks(socks_address):
+    headers = f"GET / HTTP/1.1\r\nHost: {target_host}\r\n\r\n"
+    socks_host, socks_port = socks_address.split(":")
+    socks_port = int(socks_port)
+
+    try:
+        socks.setdefaultproxy(proxy_type_list[proxy_type], socks_host, socks_port)
+    except:
+        return
+
+    try:
+        conn = socks.socksocket()
+        conn.settimeout(5)
+        conn.connect((target_host, target_port))
+
+        if target_port == 443:
+            ssl_context = ssl.SSLContext()
+            conn = ssl_context.wrap_socket(conn, server_hostname=target_host)
+
+        conn.send(headers.encode())
+        conn.close()
+
+        with lock:
+            live_socks.append(socks_address)
+
+    except:
+        pass
+
+if __name__ == "__main__":
+    proxy_type = sys.argv[1]
+    input_file = "proxy.txt"
+    output_file = "proxy.txt"
+
+    with open(input_file) as file:
+        total_socks = [line.strip() for line in file]
+
+    live_socks = []
+    lock = threading.Lock()
+
+    threads = []
+    for socks_address in total_socks:
+        thread = threading.Thread(target=check_socks, args=(socks_address,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    with open(output_file, "w") as file:
+        for socks_address in live_socks:
+            file.write(socks_address + "\n")
+
+    print('Total SOCKS working:', len(live_socks))
