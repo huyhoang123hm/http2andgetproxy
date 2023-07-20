feef9ba7c5ac1ee1939c22ae3bdb7c5d5871b20a
huyhoang123hm <78525207+huyhoang123hm@users.noreply.github.com>
2023-07-20 14:55:12 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,52 +1,72 @@
 #!/usr/bin/env python3
 
-import socket
-import psutil
-import time
 import subprocess
+import time
 
-def get_network_stats():
-    # Get the network stats for the specified interface (e.g., eth0)
-    net_interface = "eth0"  # Replace with your network interface
-    net_stats = psutil.net_io_counters(pernic=True).get(net_interface)
-    return net_stats
+block_threshold_connections = 50    # Set the threshold for blocking an IP based on connections (adjust as needed)
+block_threshold_packets = 500       # Set the threshold for blocking an IP based on packets (adjust as needed)
+block_duration = 3600               # Set the duration (in seconds) to block an IP (1 hour in this example)
+blocked_ips_connections = {}        # Dictionary to store blocked IP addresses based on connections and timestamps
+blocked_ips_packets = {}            # Dictionary to store blocked IP addresses based on packets and timestamps
 
-def get_mbps(stats):
-    # Calculate the network traffic in Mbps
-    return (stats.bytes_sent + stats.bytes_recv) * 8 / 1000000
+def get_ssh_connections_count():
+    # Get the number of SSH connections per IP address
+    ssh_connections = subprocess.run("sudo netstat -tnp | grep ':22' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c", shell=True, capture_output=True, text=True)
+    ssh_connections = ssh_connections.stdout.strip().splitlines()
+    connections = {}
+    for conn in ssh_connections:
+        count, ip = conn.split()
+        connections[ip] = int(count)
+    return connections
 
-def get_pps(stats):
-    # Calculate the packets per second
-    return stats.packets_sent + stats.packets_recv
+def get_ssh_packet_count():
+    # Get the number of SSH packets per IP address
+    ssh_packets = subprocess.run("sudo tcpdump -n -i eth0 -s 0 -c 1000 port 22 2>/dev/null | awk '{print $3}' | cut -d. -f1-4 | sort | uniq -c", shell=True, capture_output=True, text=True)
+    ssh_packets = ssh_packets.stdout.strip().splitlines()
+    packets = {}
+    for pkt in ssh_packets:
+        count, ip = pkt.split()
+        packets[ip] = int(count)
+    return packets
 
 def block_ip(ip):
     # Add an 'iptables' rule to block incoming traffic from the specified IP
     subprocess.run(f"sudo iptables -A INPUT -s {ip} -j DROP", shell=True)
-    print(f"Blocked {ip} for suspicious traffic.")
+    print(f"Blocked {ip} for excessive SSH connections and/or packets.")
 
-def detect_ddos(mbps_threshold, pps_threshold):
+def unblock_ip(ip):
+    # Remove the 'iptables' rule to unblock the IP
+    subprocess.run(f"sudo iptables -D INPUT -s {ip} -j DROP", shell=True)
+    print(f"Unblocked {ip} after {block_duration} seconds.")
+
+def detect_excessive_ssh_connections_and_packets():
     while True:
-        net_stats = get_network_stats()
-        mbps = get_mbps(net_stats)
-        pps = get_pps(net_stats)
-
-        print(f"Mbps: {mbps:.2f} | PPS: {pps}")
-
-        if mbps > mbps_threshold or pps > pps_threshold:
-            print("DDoS detected!")
-            # Get a list of all source IP addresses in the '/var/log/auth.log' file
-            cmd = "grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' /var/log/auth.log | sort | uniq"
-            result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-            ips = set(result.stdout.strip().splitlines())
-            
-            # Block all suspicious IPs
-            for ip in ips:
+        connections = get_ssh_connections_count()
+        for ip, count in connections.items():
+            if count > block_threshold_connections and ip not in blocked_ips_connections:
                 block_ip(ip)
+                blocked_ips_connections[ip] = time.time()
+
+        packets = get_ssh_packet_count()
+        for ip, count in packets.items():
+            if count > block_threshold_packets and ip not in blocked_ips_packets:
+                block_ip(ip)
+                blocked_ips_packets[ip] = time.time()
+
+        # Check if it's time to unblock IPs based on connections
+        current_time = time.time()
+        for ip, timestamp in list(blocked_ips_connections.items()):
+            if current_time - timestamp >= block_duration:
+                unblock_ip(ip)
+                del blocked_ips_connections[ip]
+
+        # Check if it's time to unblock IPs based on packets
+        for ip, timestamp in list(blocked_ips_packets.items()):
+            if current_time - timestamp >= block_duration:
+                unblock_ip(ip)
+                del blocked_ips_packets[ip]
 
         time.sleep(1)  # Wait for 1 second before checking again
 
 if __name__ == "__main__":
-    mbps_threshold = 100  # Set the Mbps threshold for DDoS detection
-    pps_threshold = 1000  # Set the pps threshold for DDoS detection
-
-    detect_ddos(mbps_threshold, pps_threshold)
+    detect_excessive_ssh_connections_and_packets()
