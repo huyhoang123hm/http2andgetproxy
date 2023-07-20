ec6d3d19336b2edc898618a905c76f49222b9d1b
huyhoang123hm <78525207+huyhoang123hm@users.noreply.github.com>
2023-07-20 15:41:17 +0700

    hhhhhhhh.py

--- hehehe.py
+++ hehehe.py
@@ -31,3 +31,33 @@ def block_ip(ip):
 
 def extract_ips_from_log(log_file_path):
     with open(log_file_path, "r") as log_file:
+        log_content = log_file.read()
+
+    # Use regular expression to extract IP addresses from the log content
+    ips = re.findall(r"\b(?:\d{1,3}\.){3}\d{1,3}\b", log_content)
+    return set(ips)
+
+def detect_ddos(mbps_threshold, pps_threshold):
+    log_file_path = "/var/log/auth.log"  # Replace with the path to your log file
+    while True:
+        net_stats = get_network_stats()
+        mbps = get_mbps(net_stats)
+        pps = get_pps(net_stats)
+
+        print(f"Mbps: {mbps:.2f} | PPS: {pps}")
+
+        if mbps > mbps_threshold or pps > pps_threshold:
+            print("DDoS detected!")
+            ips = extract_ips_from_log(log_file_path)
+
+            # Block all suspicious IPs
+            for ip in ips:
+                block_ip(ip)
+
+        time.sleep(1)  # Wait for 1 second before checking again
+
+if __name__ == "__main__":
+    mbps_threshold = 100  # Set the Mbps threshold for DDoS detection
+    pps_threshold = 1000  # Set the pps threshold for DDoS detection
+
+    detect_ddos(mbps_threshold, pps_threshold)
